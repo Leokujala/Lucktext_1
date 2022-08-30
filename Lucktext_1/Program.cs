b4dbf12f5415f9b29e6e84b2780187e2babb3ce8
Leo.Kujala <Leo.kujala@elev.ga.ntig.se>
2022-08-30 10:26:40 +0200

    initial commit

---
+++ Lucktext_1/Program.cs
@@ -0,0 +1,21 @@
+﻿string name = "";
+string food = "";
+string drink = "";
+
+
+
+Console.WriteLine ("Hej, vad heter du?");
+name = Console.ReadLine ();
+
+Console.WriteLine ("Nämn en udda sak att äta.");
+food = Console.ReadLine ();
+
+Console.WriteLine ("Nämn en udda sak att dricka.");
+drink = Console.ReadLine ();
+
+Console.WriteLine ($"Tja {name}, du måste sluta med att äta {food}. En dag kommer det ta kol på dig.");
+Console.WriteLine ($"Man kan säga detsamma om hur mycket {drink} du häller i dig! ");
+
+
+
+Console.ReadLine();
